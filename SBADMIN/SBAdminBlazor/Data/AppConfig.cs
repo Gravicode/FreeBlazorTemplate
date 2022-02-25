8f17d8906ea90ea494f74b985b276baf2407c9f0
mifmasterz <mifmasterz@yahoo.com>
2022-02-26 06:51:58 +0700

    add more

--- SBADMIN/SBAdminBlazor/Data/AppConfig.cs
+++ SBADMIN/SBAdminBlazor/Data/AppConfig.cs
@@ -2,6 +2,11 @@
 {
     public class AppConfig
     {
+        //public enum ThemeStyles
+        //{
+        //    Dark,Light
+        //}
+        //public static ThemeStyles CurrentTheme = ThemeStyles.Light;
         public static string AppName = "SB Admin Template";
     }
 }
